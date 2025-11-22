373c96637031e54f3a8ca14e42b12ec45dca6f63
Deividgaston <157326724+Deividgaston@users.noreply.github.com>
2025-11-22 10:32:13 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,13 @@
 // firebase-config.js
-// Lee la API key desde el navegador (localStorage)
-// y usa el resto de datos fijos de tu proyecto Firebase.
+// Config fijo de Firebase (esta apiKey NO es secreta en apps web).
 
 export function getFirebaseConfig() {
-  const apiKey = localStorage.getItem("firebaseApiKey");
-
-  if (!apiKey) {
-    throw new Error(
-      "No se ha encontrado la API key en el navegador. " +
-      "Pulsa en 'Configurar API Key de Firebase' en la página principal."
-    );
-  }
-
   return {
-    apiKey,
-    // ⬇️ RELLENA ESTOS CAMPOS CON LOS DATOS DE TU PROYECTO FIREBASE
+    apiKey: "AIzaSyBVbw5KS6VPc3P16B_OIPQgRYr-r2kE_tw",
     authDomain: "escaner-tarjetas.firebaseapp.com",
-  projectId: "escaner-tarjetas",
-  storageBucket: "escaner-tarjetas.firebasestorage.app",
-  messagingSenderId: "879669401384",
-  appId: "1:879669401384:web:33a94e2e77610120b0221d",
+    projectId: "escaner-tarjetas",
+    storageBucket: "escaner-tarjetas.firebasestorage.app",
+    messagingSenderId: "879669401384",
+    appId: "1:879669401384:web:33a94e2e77610120b0221d",
   };
 }
